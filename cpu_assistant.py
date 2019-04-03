5a35450f0a9981f52bbb547dc5691500f2337d2c
Matt Galloway <mattgalloway22@me.com>
2019-04-03 09:57:38 -0500

    Make CPU assistant more granular

--- cpu_assistant.py
+++ cpu_assistant.py
@@ -26,14 +26,21 @@ class CPUAssistant(Assistant):
         Assistant.__init__(self, name, delay, zone_id, is_muted)
         self.process_name: str = process_name
         self.OFF: str = 'off'
-        self.ON: str = 'on'
+        self.LOW: str = 'low'
+        self.MEDIUM: str = 'medium'
+        self.HIGH: str = 'high'
 
     def state_identifier(self) -> str:
         try:
             for process in process_iter():
-                if (self.process_name.lower() in process.name().lower() and
-                   process.cpu_percent(interval=self.delay) > 0):
-                    return self.ON
+                if self.process_name.lower() in process.name().lower():
+                    cpu_pct: float = process.cpu_percent(interval=self.delay)
+                    if 0.0 < cpu_pct <= 50.0:
+                        return self.LOW
+                    elif 50.0 < cpu_pct <= 100.0:
+                        return self.MEDIUM
+                    elif 100.0 < cpu_pct:
+                        return self.HIGH
             return self.OFF
         except (AccessDenied, NoSuchProcess):
             return self.OFF
@@ -41,7 +48,9 @@ class CPUAssistant(Assistant):
     def color_identifier(self, state: str) -> str:
         color_switcher: Dict[str, str] = {
             self.OFF: COLORS['red'],
-            self.ON: COLORS['light green']
+            self.LOW: COLORS['orange'],
+            self.MEDIUM: COLORS['yellow'],
+            self.HIGH: COLORS['light green']
         }
         if state in color_switcher:
             return color_switcher[state]
@@ -51,7 +60,9 @@ class CPUAssistant(Assistant):
     def message_identifier(self, state: str) -> str:
         message_switcher: Dict[str, str] = {
             self.OFF: self.name + ' has turned off',
-            self.ON: self.name + ' has turned on'
+            self.LOW: self.name + ' is running (low)',
+            self.MEDIUM: self.name + ' is running (medium)',
+            self.HIGH: self.name + ' is running (high)'
         }
         if state in message_switcher:
             return message_switcher[state]
