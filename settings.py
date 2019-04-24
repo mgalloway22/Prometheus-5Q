4c671e856379089144ff02c6fac50d069baebebc
Matt Galloway <mattgalloway22@me.com>
2019-04-24 12:16:12 -0500

    Reformat colors to be easier to differentiate on the keyboard and turn off debug mode

--- settings.py
+++ settings.py
@@ -3,19 +3,19 @@ from typing import Dict
 """General class for commonly used variables
 """
 
-IS_DEBUG_MODE: bool = True
+IS_DEBUG_MODE: bool = False
 BASE_URL: str = 'http://localhost:27301/api/1.0/signals'
 PID: str = 'DK5QPID'
 HEADERS: Dict[str, str] = {'Content-type': 'application/json'}
 COLORS: Dict[str, str] = {
     'red': '#CC0000',
-    'orange': '#E67E22',
-    'yellow': '#F1C40F',
+    'orange': '#FF8000',
+    'yellow': '#FFFF00',
     'light green': '#00CC00',
-    'dark green': '#186A3B',
-    'light blue': '#3498DB',
-    'dark blue': '#1B4F72',
-    'purple': '#9900CC',
-    'pink': '#EC7063',
+    'dark green': '#00FF00',
+    'light blue': '#0000CC',
+    'dark blue': '#0000FF',
+    'purple': '#330033',
+    'pink': '#FF0066',
     'error': '#FFFFFF',
 }
